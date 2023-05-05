c8d00c192ebcb774606b8b85e8101b5e925be1d4
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-05 11:26:44 +1000

    done with lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram 
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        matched_word = arr.filter { |str| @word.chars.sort == str.chars.sort }
+        matched_word
+    end
+
+end
